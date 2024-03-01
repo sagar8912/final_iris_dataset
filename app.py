a954d7e739ff8aff31d26a121cc718ec9bb1fc14
sagar8912 <160304264+sagar8912@users.noreply.github.com>
2024-03-01 17:18:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def predict():
         return render_template('index.html', prediction_text='Error processing input')
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=False)
 
